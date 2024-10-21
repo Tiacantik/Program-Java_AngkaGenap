ae4677c20468248b16f32b1c97e3f1395d53b3dd
Tiacantik <mutiara06pobela@gmail.com>
2024-10-21 19:40:09 +0700

    Create AngkaGenap.java
    Memnambah program untuk mencetak angka genap dari 1 hingga 20.

---
+++ AngkaGenap.java
@@ -0,0 +1,10 @@
+public class AngkaGenap {
+    public static void main(String[] args) {
+        System.out.println("Angka genap dari 1 sampai 20:");
+        for (int i = 1; i <= 20; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
